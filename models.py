d30bd60b1edfc204cc955ad20f2a7d014109a713
O.V. Rud <oleg.rud@natur.cuni.cz>
2024-09-13 18:21:14 +0200

    added new field to database model. wind direction as string

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import Column, Integer, Float, String, create_engine
+from sqlalchemy import Column, String, Integer, Float, DateTime, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # This code creates an sqlite database and prepare the columns
@@ -10,11 +10,19 @@ Base = declarative_base()
 class Weather(Base):
     __tablename__ = "weather"
     id = Column(Integer, primary_key=True)
+    time = Column(DateTime)
     temperature_2m = Column(Float)
     surface_pressure = Column(Float)
     wind_speed_10m = Column(Float) 
     wind_direction_10m = Column(Float)
+    wind_direction = Column(String)
     precipitation = Column(Float)
+    interval = Column(Integer) 
+    
 
-if os.path.   
-Base.metadata.create_all(engine)
+if not os.path.exists("weatherdb.sqlite"):
+    Base.metadata.create_all(engine)
+    
+    
+    
+    
